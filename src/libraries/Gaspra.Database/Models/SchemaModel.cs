726e2dfd0b04572b7b10d23a5dab07d6841ae272
Richard Lawrence <35634732+Gaspra@users.noreply.github.com>
2021-03-15 14:13:42 +0000

    start building database models

--- src/libraries/Gaspra.Database/Models/SchemaModel.cs
+++ src/libraries/Gaspra.Database/Models/SchemaModel.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Gaspra.Database.Models
 {
     public class SchemaModel
     {
+        public string Name { get; set; }
+        public IReadOnlyCollection<TableModel> Tables { get; set; }
     }
 }
--- src/libraries/Gaspra.Database/Services/DatabaseStructure.cs
+++ src/libraries/Gaspra.Database/Services/DatabaseStructure.cs
@@ -2,15 +2,16 @@
 using Gaspra.Database.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Gaspra.Database.Services
 {
     public class DatabaseStructure : IDatabaseStructure
     {
-        public Task<DatabaseModel> CalculateStructure(IReadOnlyCollection<ColumnModel> columns, IReadOnlyCollection<ConstraintModel> constraints, IReadOnlyCollection<PropertyModel> properties)
+        public Task<DatabaseModel> CalculateStructure(
+            IReadOnlyCollection<ColumnModel> columns,
+            IReadOnlyCollection<ConstraintModel> constraints,
+            IReadOnlyCollection<PropertyModel> properties)
         {
             throw new NotImplementedException();
         }
